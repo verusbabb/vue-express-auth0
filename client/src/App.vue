01f37e841ff4faac292c1e347f86b30006fad653
Steve Babb <stevebabb@Steves-MacBook-Pro-2.local>
2022-07-11 20:38:38 -0500

    minor updates to client/needs version updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="sidebar" app>
-      <v-list>
-        <v-list-tile
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path"
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-toolbar app>
       <span class="hidden-sm-and-up">
-        <v-toolbar-side-icon @click="sidebar = !sidebar"> </v-toolbar-side-icon>
+        <v-app-bar-nav-icon @click="sidebar = !sidebar"> </v-app-bar-nav-icon>
       </span>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">
@@ -26,7 +11,7 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path">
+        <v-btn v-for="item in menuItems" :key="item.title" :to="item.path">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
@@ -46,7 +31,7 @@
     name: "App",
     data() {
       return {
-        appTitle: "Awesome App",
+        appTitle: "Alpha Nu",
         sidebar: false,
         menuItems: [
           { title: "Home", path: "/home", icon: "home" },
